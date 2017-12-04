77a685ff009cd9d3465dd988203ff9ffa09ce08f
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-04 17:32:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+
+
+
+def oxford_comma(array) #method takes an argument of array
+  if array.length == 2 # if the array is equal to 2
+    return "#{array[0]} and #{array[1]}" # then return with "and"
+  elsif 2 < array.length # if the length of array is only 1
+    array[-1].insert(0, "and ") #the subtract the "and"
+  end
+  array.join(", ") #this converts the array to a string and adds in a comma 
 end
